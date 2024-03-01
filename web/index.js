44ab6b28a418c10593c878c8bf9cd91873a72dda
Richard <richard.m.hpa@gmail.com>
2024-03-01 17:13:39 +1300

    set up initial config

--- web/index.js
+++ web/index.js
@@ -3,28 +3,27 @@ const mysql = require('mysql');
 const app = express();
 
 const connection = mysql.createPool({
-	connectionLimit: 10,
-	host: process.env.MYSQL_HOST || 'localhost',
-	user: process.env.MYSQL_USER || 'root',
-	password: process.env.MYSQL_PASSWORD || 'password',
-	database: process.env.MYSQL_DATABASE || 'test'
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'test',
 });
 
 app.get('/', (req, res) => {
-	connection.query('SELECT * FROM Student' , (err, rows) => {
-		if(err){
-			res.json({
-				success: false,
-				err
-				});
-		}
-		else{
-			res.json({
-				success: true,
-				rows
-				});
-		}
-	});
+  connection.query('SELECT * FROM Student', (err, rows) => {
+    if (err) {
+      res.json({
+        success: false,
+        err,
+      });
+    } else {
+      res.json({
+        success: true,
+        rows,
+      });
+    }
+  });
 });
 
-app.listen(5000, () => console.log('listining on port 5000'));
+app.listen(5001, () => console.log('listening on port 5001'));
